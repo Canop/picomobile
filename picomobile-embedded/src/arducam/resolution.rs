f998b72e53dc5d285cd91f70b3c8cc42a7bcb6e0
Canop <cano.petrole@gmail.com>
2026-06-24 16:45:08 +0200

    camera resolution set to requested one

--- picomobile-embedded/src/arducam/resolution.rs
+++ picomobile-embedded/src/arducam/resolution.rs
@@ -1,8 +1,13 @@
-use super::ov2640_registers::*;
+use {
+    super::ov2640_registers::*,
+    core::str::FromStr,
+};
 
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub enum Resolution {
     R160x120,
     R320x240,
+    #[default]
     R400x296,
     R640x480,
     R1024x768,
@@ -16,6 +21,21 @@ impl Resolution {
             Resolution::R400x296 => OV2640_400x296_JPEG,
             Resolution::R640x480 => OV2640_640x480_JPEG,
             Resolution::R1024x768 => OV2640_1024x768_JPEG,
+        }
+    }
+}
+
+impl FromStr for Resolution {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "160x120" => Ok(Resolution::R160x120),
+            "320x240" => Ok(Resolution::R320x240),
+            "400x296" => Ok(Resolution::R400x296),
+            "640x480" => Ok(Resolution::R640x480),
+            "1024x768" => Ok(Resolution::R1024x768),
+            _ => Err("Invalid resolution string"),
         }
     }
 }
